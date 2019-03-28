fc90327efb1663ba242235488d88fa7bfe413237
aiden013827 <aiden@neuralbc.com>
2019-03-28 17:33:11 +0900

    client testDev first setting

---
+++ src/components/App/App.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <a
+            className="App-link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Project List
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
