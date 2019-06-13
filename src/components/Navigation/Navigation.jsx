10714baa9b9aa367329187687783b33b02493000
arsendev <arsen@clo3d.com>
2019-06-13 12:52:18 +0900

    scroll event modify

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,15 +1,25 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { easeInOutQuad } from 'ez.js';
 
 export const Navigation = ({ recent, logo }) => {
 
-  const naviFocusMoveEvent = (recent, () => {
-    window.scrollTo({
-      top: recent.current.offsetTop - 50,
-      left: 0,
-      behavior: 'smooth'
-    });
-  });
+  const naviFocusMoveEvent = (target, duration) => {
+    let startPosition = window.scrollY || window.pageYOffset;
+    let targetPosition = target.current.offsetTop - 50;
+    let distance = targetPosition - startPosition;
+    let timeStart = null;
+
+    const loop = timeCurrent => {
+      if (timeStart === null) timeStart = timeCurrent;
+      let timeElapsed = timeCurrent - timeStart;
+      let next = easeInOutQuad(timeElapsed, startPosition, distance, duration);
+      window.scrollTo(0, next);
+      if (timeElapsed < duration) window.requestAnimationFrame(loop);
+    }
+
+    window.requestAnimationFrame(loop);
+  };
 
   return (
     <header>
@@ -19,13 +29,12 @@ export const Navigation = ({ recent, logo }) => {
             <h1>
               <Link to="/">
                 <img src={logo} alt="logo-image" draggable="false" />
-                {/* <em>FE Project</em> */}
               </Link>
             </h1>
           </div>
           <div className="nav-menu">
             <ul>
-              <li onClick={naviFocusMoveEvent.bind(this)}>Front-End Project</li>
+              <li onClick={ () => naviFocusMoveEvent(recent,500)}>Front-End Project</li>
               <li><a href="http://seokweb.com/portfolio/" target="_blank">Publisher Project</a></li>
             </ul>
           </div>
