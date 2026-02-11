56f5946c74a941c765ca0e339ed4c92218ef9787
seoksanghwan <bdoi16@naver.com>
2026-02-11 17:19:20 +0900

    :hammer: 프로젝트 패키지 전환, 리액트 업데이트 및  js -> ts로 변경

--- src/index.tsx
+++ src/index.tsx
@@ -2,22 +2,18 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import AppContainer from './container/App';
-import reportWebVitals from './reportWebVitals'; // 추가
-import './common.scss'; //
-
-// FontAwesome 설정 생략 (기존 유지)
+import './common.scss';
 
 const container = document.getElementById('root');
+
 if (container) {
   const root = createRoot(container);
   root.render(
     <React.StrictMode>
-      <BrowserRouter>
+      {/* 상환님의 요청대로 basename을 /로 고정합니다. */}
+      <BrowserRouter basename="/">
         <AppContainer />
       </BrowserRouter>
     </React.StrictMode>,
   );
 }
-
-// 성능 측정을 원한다면 결과를 콘솔에 출력하거나 엔드포인트로 전송
-reportWebVitals();
