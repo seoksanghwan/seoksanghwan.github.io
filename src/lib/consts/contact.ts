db17924cb920277a5577f2dbc5d1c7ac04c3245c
seoksanghwan <bdoi16@naver.com>
2026-02-20 13:28:39 +0900

    :hammer: 링크드인 링크 수정

--- src/lib/consts/contact.ts
+++ src/lib/consts/contact.ts
@@ -2,5 +2,5 @@ export const contact = {
   email: 'bdoi2214@gmail.com',
   phone: '010-4921-7224',
   github: 'https://github.com/seoksanghwan',
-  linkedin: 'https://www.linkedin.com/...',
+  linkedin: 'https://www.linkedin.com/in/sanghwan-seok-202938171',
 } as const;
